e912f8ee82e9b3c9161cbf3cf5b2d011759660c4
shreytheshreyas <59922045+shreytheshreyas@users.noreply.github.com>
2021-03-31 01:30:15 +0800

    Create Led_Control.h

---
+++ Led_Control.h
@@ -0,0 +1,12 @@
+//Include guard
+#ifndef LED_H   
+#define LED_H
+
+void led_stationary_display(void);
+void led_moving_display(void);
+void flashTwiceGreen(void);
+void initGPIO(void);
+void turnOffGreen(void);
+void turnOffRed(void);
+
+#endif
